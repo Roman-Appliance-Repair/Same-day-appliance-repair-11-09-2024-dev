1c722f4f72347b7dab40cffdf049e9b8476259ab
null <null>
2025-01-19 10:37:56 -0800

    1

--- src/pages/ga-tracking.js
+++
@@ -1,5 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-F5MBY27XES', { 'anonymize_ip': true, 'send_page_view': false });
