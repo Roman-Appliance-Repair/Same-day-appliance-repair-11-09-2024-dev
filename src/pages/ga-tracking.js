fc0ba32e8d5a67e114d0a6dd45c032d1a4545548
null <null>
2025-01-19 10:28:17 -0800

    1

--- src/pages/ga-tracking.js
+++ src/pages/ga-tracking.js
@@ -1,13 +1,5 @@
-(function() {
-    var script = document.createElement("script");
-    script.src = "/js/gtag.js";
-    script.async = true;
-    document.head.appendChild(script);
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-    script.onload = function() {
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-F5MBY27XES', { 'anonymize_ip': true, 'send_page_view': false });
-    };
-})();
+gtag('config', 'G-F5MBY27XES', { 'anonymize_ip': true, 'send_page_view': false });
