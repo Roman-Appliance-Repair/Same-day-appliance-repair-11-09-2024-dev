bc0b080d08ef35c4722053dc4e71c8da0c89b4b1
robot-sova <6195738869abysov@gmail.com>
2025-11-14 11:54:32 -0800

    5

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,8 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://samedayappliance.repair",
-
-  experimental: {
-    inlineStylesheets: "always",
-  },
-
   integrations: [tailwind(), sitemap()],
 });
-
 
 
 
